0860ec5149ec88bfea0d84fba269d90a1ff36ea1
nuhla <nuhlamasri@gmail.com>
2019-11-08 14:44:46 +0200

    adding html

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const port = process.env.PORT ||3000;
 const path = require('path')
 
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) =>console.log("hi"));
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('public/index'))
-  .set('view engine', 'jsx')
-  .listen(port, () => console.log(`Listening on ${ port }`))
+app.listen(port, () => console.log(`Listening on ${ port }`))
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .get('/', (req, res) => res.render('public/index'))
+//   .set('view engine', 'jsx')
+//   .listen(port, () => console.log(`Listening on ${ port }`))
 
