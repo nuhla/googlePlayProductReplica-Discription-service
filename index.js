900c0e08bdd684eade8d14be33f7abebea16c262
nuhla <nuhlamasri@gmail.com>
2019-11-08 11:57:38 +0200

    h

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
+const port = 3000;
+app.connect()
 const app = express();
+app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
 
-    res.render('main.html');
-})
+// app.get('/', (req, res)=>{
+
+//     res.render('main.html');
+// })
+
+app.listen(port, function() {
+    console.log(`listening on port ${port}`);
+  });
+
