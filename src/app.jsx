4dc5ec00024c34a68febdec8a6eb269fb139f350
nuhla <nuhlamasri@gmail.com>
2019-11-13 16:31:28 +0200

    chang to defualt value and unfounded item

--- src/app.jsx
+++ src/app.jsx
@@ -35,27 +35,34 @@ const Divall = styled.div`
   padding: 0.25em 1em;
   font-family: Scheherazade, Arial, Helvetica, sans-serif;
   line-height: 3.6;
-
   display: block;
   height: 100%;
   text-overflow: ellipsis;
   white-space: pre-line;
 `;
+const NotFounded = styled.p`
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  font-size: 2.2em;
+  color: #ada5a5;
+  display: block;
+  text-overflow: ellipsis;
+  padding: 0.25em 1em;
+  padding: 5px;
+  height: 100%;
+`;
 
 const InnerDv = styled.button`
-  font-size: 1.5em;
+  font-size: 1.2em;
   padding: 0.25em 1em;
   border-radius: 0px;
-  border: 0px
-  border-radius: 0px;
-
+  border: none;
+  background: none;
   width: 100%;
-  height:80px;
-  border-radius: 20px 10px;
-  background-color:#ffffff;
-  border-top: 0px ;
-
-  color: green;`;
+  height: 65px;
+  border-radius: 5px 10px;
+  color: #33691e;
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -81,7 +88,7 @@ class App extends React.Component {
   componentDidMount() {
     var itemId = 1;
     var windowurl = window.location.href;
-    if (windowurl.indexOf("itemid") > -1) {
+    if (windowurl.indexOf("itemid") > 0) {
       itemId = windowurl.substring(
         windowurl.indexOf("itemid") + "itemid".length + 1,
         windowurl.length
@@ -110,10 +117,12 @@ class App extends React.Component {
         datatype: "apllication/json",
         success: function(response) {
           // putting the value of success response to this comp state
-          that.setState({
-            userId: response.data.userID,
-            Discription: response.data.description
-          });
+          if (response.data) {
+            that.setState({
+              userId: response.data.userID,
+              Discription: response.data.description
+            });
+          }
         },
         error: function(error) {
           alert(error);
@@ -150,11 +159,19 @@ class App extends React.Component {
         {/* if the state of read more is true then  load styled-component of hiegt 100px%  */}
         {!this.state.openreadmoe ? (
           <Div>
-            <p>{this.state.Discription}</p>
+            {this.state.Discription ? (
+              <p> {this.state.Discription} </p>
+            ) : (
+              <NotFounded> Item Not Founded</NotFounded>
+            )}
           </Div>
         ) : (
           <Divall>
-            <p>{this.state.Discription}</p>
+            {this.state.Discription ? (
+              <p> {this.state.Discription} </p>
+            ) : (
+              <NotFounded> Item Not Founded</NotFounded>
+            )}
           </Divall>
         )}
         {/* if the state of read more is false then text will be less */}
