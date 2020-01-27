2c0357eb34e902133bee68c06673f8a9a18f36a2
nuhla <nuhlamasri@gmail.com>
2020-01-27 19:17:38 +0200

    git ignore

--- config/db-key.js
+++ config/db-key.js
@@ -1,14 +1,13 @@
 module.exports = {
   mongoURI: process.env.DATA_PATH,
-  // "mongodb+srv://FINZ:1111@finz-0pho2.mongodb.net/Disciption?retryWrites=true&w=majority",
 
   monoCFG: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     ssl: true,
-    replicaSet: "FINZ- shard - 0",
-    authSource: "admin",
+    replicaSet: 'FINZ- shard - 0',
+    authSource: 'admin',
     retryWrites: true,
-    dbName: "Products"
+    dbName: 'Products'
   }
 };
