721b9760c9f3058b8d02f8e54da9a4a82eec9f28
nuhla <nuhlamasri@gmail.com>
2020-01-28 04:58:13 +0200

    add comments to server

--- db/db.js
+++ db/db.js
@@ -1,18 +1,24 @@
-const mongoose = require("mongoose");
-const config = require("../config/keys");
+//------------------------------------------------------------------------------------//
+//-----------------Importing required libraries into our file ------------------------//
+//------------------------------------------------------------------------------------//
+const mongoose = require('mongoose');
+const config = require('../config/keys');
 
+//-------------------------------------------------------------------------------//
+//---------------------------- Connecting to Our DataBase------------------------//
+//-------------------------------------------------------------------------------//
 mongoose
   .connect(config.mongoURI, config.monoCFG)
   .catch(() => {
-    console.log("no connection", error);
+    console.log('no connection', error);
   })
   .then(() => {
-    console.log("CONNECTED");
+    console.log('CONNECTED');
   });
 
-// var Schema = mongoose.Schema;
-
-//Using Schema constructor, create a ProductSchema
+//-------------------------------------------------------------------------------//
+// -------------Using Schema constructor, create a ProductSchema ----------------//
+//-------------------------------------------------------------------------------//
 var ProductSchema = new mongoose.Schema({
   userID: Number,
   description: String,
@@ -22,39 +28,37 @@ var ProductSchema = new mongoose.Schema({
   }
 });
 
-var ExampleData = {
-  userID: 1,
-  description: "ddddddddddddd",
-  updateDate: Date.now()
-};
-console.log(Date.now());
+//-------------------------------------------------------------------------------//
+// ----- Creating a model from the schema to use it in insertion .. exrt---------//
+//-------------------------------------------------------------------------------//
+var Product = mongoose.model('Product', ProductSchema);
 
-var Product = mongoose.model("Product", ProductSchema);
-
-/// function to save the new value for this item dicription
+//-------------------------------------------------------------------------------------//
+//----------------- function to save the new value for this item dicription -----------//
+//-------------------------------------------------------------------------------------//
 var save = function(data) {
-  // console.log(data);
   var newProduct = new Product(data);
   newProduct.save((error, product) => {
     if (error) {
-      console.log("error", err);
+      console.log('error', err);
     } else {
       console.log(product);
     }
   });
 };
-
-// save(ExampleData);
-//a function to find the disciption of this item
+//----------------------------------------------------------------------------------------//
+//------------------a function to find the disciption of this item------------------------//
+//----------------------------------------------------------------------------------------//
 var findOne = function(obj, callback) {
-  Product.findOne(obj, "disciption", function(err, person) {
-    if (err) console.log(err, "this a database error");
-    // Prints "Space Ghost is a talk show host".
-    else console.log("%s %s is a %s.", Product.disciption);
+  Product.findOne(obj, 'disciption', function(err, person) {
+    if (err) console.log(err, 'this a database error');
+    else console.log('%s %s is a %s.', Product.disciption);
   });
 };
 
-//eporting all needed attributes in this model
+//------------------------------------------------------------------------------------------------------//
+//------------------Exporting all needed Sechemas and functions to be used later -----------------------//
+//------------------------------------------------------------------------------------------------------//
 module.exports.Product = Product;
 module.exports.save = save;
 module.exports.findOne = findOne;
